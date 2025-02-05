3de0967b51c2299f7b221676862d3bc99fd94b21
EC2 Default User <ec2-user@ip-192-168-8-137.ec2.internal>
2025-02-05 14:04:20 +0000

    sdg

--- run_playbook.sh
+++ run_playbook.sh
@@ -1,1 +1,1 @@
-ansible-playbook main_playbook.yml -i aws_ec2.yml --vault-password-file group_vars/all/vault.pass
+ansible-playbook main_playbook.yml -i aws_ec2.yml --vault-password-file group_vars/all/vault.pass -vvv
